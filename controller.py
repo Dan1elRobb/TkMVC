7c905c6cc180453f5fda886bd018bfbd0c427427
danrob0910 <danrob0910@highgateschool.org.uk>
2023-03-06 11:59:21 +0000

    now works in sending to db from console

--- controller.py
+++ controller.py
@@ -6,15 +6,15 @@ class Controller:
     def __init__(self):
         self.engine = create_engine('sqlite:///emails.sqlite', echo=True)
 
-    def save(self, email):
-        email_address = EmailAddress(email)
+    def save(self, p_email):
+        email_address = EmailAddress(address=p_email)
         try:
             with Session(self.engine) as sess:
                 sess.add(email_address)
                 sess.commit()
 
             # show a success message
-            return f'The email {email} saved! to database'
+            return f'The email {p_email} saved! to database'
 
         except ValueError as error:
             # show an error message
